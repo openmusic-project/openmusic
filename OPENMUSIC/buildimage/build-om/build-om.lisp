7fa4c0bd4b47c7f61b67aa944865b8d6c34916c9
andersvi <andersvi@bccda38a-22f4-47c8-a5a1-d3050c704649>
2013-03-05 11:31:41 +0000

    add check on files=*compile-type* for #'clean-sources to use when deleting cruft

--- OPENMUSIC/buildimage/build-om/build-om.lisp
+++ OPENMUSIC/buildimage/build-om/build-om.lisp
@@ -142,7 +142,8 @@
               (if (system::directory-pathname-p file)
                   (clean-sources file)
                 (when (and (pathname-type file)
-                           (find (pathname-type file) '("xfasl" "fasl" "DS_STORE" "nfasl" "ofasl" "ufasl" "lisp~") :test 'string-equal))
+                           (or (find (pathname-type file) '("xfasl" "fasl" "DS_STORE" "nfasl" "ofasl" "ufasl" "lisp~") :test 'string-equal)
+			       (string= (pathname-type file) *compile-type*))) ; remove compiled files
                   (print (concatenate 'string "Deleting " (namestring file) " ..."))
                   (delete-file file)
                   )))
