8cca7bd76e9fcd7da50ca3b7f82168c9c89a1638
andersvi <andersvi@bccda38a-22f4-47c8-a5a1-d3050c704649>
2013-05-29 14:25:27 +0000

    moved all JACK stuff from externals/MIDI/JACK to externals/JACK

---
+++ OPENMUSIC/code/api/externals/JACK/cl-jack.lisp
@@ -0,0 +1,208 @@
+;;===========================================================================
+;;JACK API for Common Lisp/CFFI
+;;
+;;This program is free software; you can redistribute it and/or modify
+;;it under the terms of the GNU Lesser General Public License as published by
+;;the Free Software Foundation; either version 2.1 of the License, or
+;;(at your option) any later version.
+;;  
+;;This program is distributed in the hope that it will be useful,
+;;but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;GNU Lesser General Public License for more details.
+;;  
+;;You should have received a copy of the GNU Lesser General Public License
+;;along with this program; if not, write to the Free Software 
+;;Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;;
+;;Author: Anders Vinjar
+
+(require :cffi "../FFI/CFFI/load-cffi")
+(defpackage :cl-jack (:use :common-lisp :cffi))
+(in-package :cl-jack)
+
+(define-foreign-library libjack
+  (t (:default "/usr/lib/libjack")))
+
+(use-foreign-library libjack) 
+
+;;; MOST OF THE BELOW IS FFI-WRAPPERS FOR THE JACK-API
+
+;;; JACK.H - CLIENTS, PORTS...
+
+(defcfun "jack_get_version_string" :string)
+;;(jack-get-version-string)
+
+(defctype size_t :unsigned-int)
+
+(defctype jack_nframes_t :uint32)
+(defctype jack_port_t :pointer)
+(defctype jack_options_t :pointer)
+(defctype jack_time_t :unsigned-long)	;:unsigned-long = :uint64?
+
+(defconstant *jack-default-audio-type* "32 bit float mono audio")
+(defconstant *jack-default-midi-type* "8 bit raw midi")
+(defctype jack_default_audio_sample_t :float)
+
+(defcenum jackoptions
+  (:jacknulloption #0x00)
+  (:jacknostartserver #0x01)
+  (:jackuseexactname #0x02)
+  (:jackservername #0x04)
+  (:JackLoadName #0x08)
+  (:JackLoadInit #0x10)
+  (:JackSessionID #0x20))
+
+(defcenum jackportflags
+  (:JackPortIsInput  #0x1)
+  (:JackPortIsOutput  #0x2)
+  (:JackPortIsPhysical  #0x4)
+  (:JackPortCanMonitor  #0x8)
+  (:JackPortIsTerminal  #0x10))
+
+(defparameter JackNullOption (foreign-enum-value 'jackoptions :jacknulloption))
+
+(defcfun "jack_client_name_size" :int)
+
+(defcvar "input_port" jack_port_t)
+
+(defcfun "jack_client_open" :pointer
+  (name :string)
+  (option :int)
+  (status :int))
+
+(defcfun "jack_get_sample_rate" :int
+  (client :pointer))
+
+(defcfun "jack_port_type_get_buffer_size" size_t
+  (client :pointer)
+  (port-type :string))
+
+(defcfun "jack_get_buffer_size" jack_nframes_t
+  (client :pointer))
+
+(defcfun "jack_get_client_name" :string
+  (client :pointer))
+
+(defcfun "jack_port_get_buffer" :pointer
+  (port :pointer)
+  (frames jack_nframes_t))
+
+(defcfun "jack_get_ports" :pointer
+  (client :pointer)
+  (port_name_pattern :string)
+  (type_name_pattern :string)
+  (flags :unsigned-long))
+
+(defcfun "jack_port_register" :pointer
+  (client :pointer)
+  (port-name :string)
+  (port-type :string)
+  (flags :unsigned-long)
+  (buffer-size :unsigned-long))
+
+(defcfun "jack_client_close" :int
+  (client :pointer))
+
+(defcfun "jack_activate" :int
+  (client :pointer))
+
+(defcfun "jack_deactivate" :int
+  (client :pointer))
+
+(defcfun "jack_set_process_callback" :int
+  (client :pointer)
+  (process_callback :pointer)
+  (arg :int))
+
+(defcfun "jack_midi_clear_buffer" :void
+  (port-buffer :pointer))
+
+(defcfun "jack_midi_event_reserve" :pointer
+  (port-buffer :pointer)
+  (time :unsigned-int)
+  (data-size :unsigned-char))
+
+
+;;; TIME
+
+(defcfun "jack_get_time" jack_time_t)
+
+(defcfun "jack_frames_to_time" jack_time_t
+  (client :pointer)
+  (frames jack_nframes_t))
+
+(defcfun "jack_time_to_frames" jack_nframes_t
+  (client :pointer)
+  (time jack_time_t))
+
+(defcfun "jack_last_frame_time" jack_nframes_t
+  (client :pointer))
+
+(defcfun "jack_frame_time" jack_nframes_t
+  (client :pointer))
+
+;;; JACK/RINGBUFFER.H
+
+(defcstruct jack_ringbuffer_t
+  (buf (:pointer :char))
+  (write-ptr size_t)
+  (read-ptr size_t)
+  (size size_t)
+  (size-mask size_t)
+  (mlocked :int))
+
+(defcstruct jack_ringbuffer_data_t
+  (buf (:pointer :float))
+  (len size_t))
+
+;; vec[0].buf
+(defun rb-data-buf (arr index)		;index is 0 or 1 from jack
+  (foreign-slot-value			
+   (mem-aref arr 'jack_ringbuffer_data_t index)
+   'jack_ringbuffer_data_t 'buf))
+
+;;vec[0].len
+(defun rb-data-len (arr index)
+  (foreign-slot-value			
+   (mem-aref arr 'jack_ringbuffer_data_t index)
+   'jack_ringbuffer_data_t 'len))
+
+;;(rb-data-len vec 0) 
+(defun rb-data-len-p (arr index)	;len=0 := nothing to get
+  (plusp (rb-data-len arr index)))
+
+(defcfun "jack_ringbuffer_create" (:pointer jack_ringbuffer_t)
+  (sz size_t))
+
+(defcfun "jack_ringbuffer_free" :void
+  (rb jack_ringbuffer_t))
+
+(defcfun "jack_ringbuffer_get_read_vector" :void
+  (rb (:pointer jack_ringbuffer_t))
+  (vec (:pointer jack_ringbuffer_data_t)))
+
+(defcfun "jack_ringbuffer_get_write_vector" :void
+  (rb (:pointer jack_ringbuffer_t))
+  (vec (:pointer jack_ringbuffer_data_t)))
+
+(defcfun "jack_ringbuffer_write_advance" :void
+  (rb (:pointer jack_ringbuffer_t))
+  (cnt size_t))
+
+(defcfun "jack_ringbuffer_read" size_t
+  (rb (:pointer jack_ringbuffer_t))
+  (dest (:pointer :char))
+  (cnt size_t))
+
+(defcfun "jack_ringbuffer_read_space" size_t
+  (rb (:pointer jack_ringbuffer_t)))
+
+;;; end of wrappers for jack/ringbuffer.h
+
+;; provide one default global client-name 
+
+(defparameter *OMJackClient* nil)
+
+(provide :cl-jack)
+
