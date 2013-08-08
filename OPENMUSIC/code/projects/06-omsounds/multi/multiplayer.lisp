0fcbb8d92776b89db4151424d165df97887f9150
bresson <bresson@bccda38a-22f4-47c8-a5a1-d3050c704649>
2013-08-08 10:56:18 +0000

    players update

--- OPENMUSIC/code/projects/06-omsounds/multi/multiplayer.lisp
+++ OPENMUSIC/code/projects/06-omsounds/multi/multiplayer.lisp
@@ -1,15 +1,15 @@
 (in-package :om)
 
 
-#+(and cocoa om-osc-api) 
-; (add-assoc-player *general-player* 'multiplayer)
-
-(defvar *multiplayer-in-port* nil)
-(setf *multiplayer-in-port* 7072)
+(defmethod player-name ((self (eql :multiplayer))) "MultiPlayer")
+(defmethod player-desc ((self (eql :multiplayer))) "external Max multi-channel player")
+(defmethod player-special-action ((self (eql :multiplayer))) (launch-multiplayer-app))
+(defmethod player-type ((player (eql :multiplayer))) :UDP)
 
 (defvar *multiplayer-out-port* nil)
 (setf *multiplayer-out-port* 7071)
-
+(defvar *multiplayer-in-port* nil)
+(setf *multiplayer-in-port* 7072)
 (defvar *multiplayer-host* nil)
 (setf *multiplayer-host* "127.0.0.1")
  
@@ -48,7 +48,7 @@
    
    (when *multiplayer-file-to-play* (player-stop :multiplayer (list *multiplayer-file-to-play*)))
    
-   (print (list "multiplayer prepare" (om-sound-file-name object)))
+   ;(print (list "multiplayer prepare" (om-sound-file-name object)))
 
    (setf *multiplayer-file-to-play* (namestring (om-sound-file-name object)))
 
@@ -80,7 +80,8 @@
    ;(om-stop-osc-server listen-answer-process)
    ;)
    (when interval
-     (om-send-osc-message *multiplayer-out-port* *multiplayer-host*  (list "/fileplayer/start" (/ (car interval) 1000.0))))
+     (let ((newinterval (om- (interval-intersec interval (list at (+ at (real-dur object)))) at)))
+     (om-send-osc-message *multiplayer-out-port* *multiplayer-host*  (list "/fileplayer/start" (/ (car newinterval) 1000.0)))))
    
    ;;; schedule the playback
    (call-next-method)
@@ -91,12 +92,12 @@
 (defmethod player-start ((engine (eql :multiplayer)) &optional play-list) (call-next-method))
 
 (defmethod player-play-object ((engine (eql :multiplayer)) (object sound) &key interval)
-  (print "multiplayer play")
+  ;(print "multiplayer play")
   (when *multiplayer-file-to-play*
     (om-send-osc-message *multiplayer-out-port* *multiplayer-host*  (list "/fileplayer/play" 1))))
 
 (defmethod player-stop ((engine (eql :multiplayer)) &optional play-list)
-  (print "multiplayer stop")
+  ;(print "multiplayer stop")
   (when *multiplayer-file-to-play*
      (om-send-osc-message *multiplayer-out-port* *multiplayer-host*  (list "/fileplayer/play" 0))
      (setf *multiplayer-file-to-play* nil)))
@@ -109,10 +110,133 @@
   (when *multiplayer-file-to-play*
     (om-send-osc-message *multiplayer-out-port* *multiplayer-host*  (list "/fileplayer/pause" 0))))
 
-;(defmethod Reset-Player ((self (eql 'multiplayer)) &optional view)
-;   (declare (ignore view))
-;   (setf *multiplayer-file-to-play* nil)
-;   t)
+
+
+;;;=========================================
+;;; PREFS
+
+;;;==================================
+;;;; ADD EXTERNAL PREF MODULE
+;;;==================================
+
+(add-external-pref-module 'multiplayer)
+
+(defmethod get-external-name ((module (eql 'multiplayer))) "MultiPlayer")
+(defmethod get-external-icon ((module (eql 'multiplayer))) 950)
+
+(defmethod get-external-module-vals ((module (eql 'multiplayer)) modulepref) (get-pref modulepref :multiplayer-options))
+(defmethod get-external-module-path ((module (eql 'multiplayer)) modulepref) (get-pref modulepref :multiplayer-path))
+(defmethod set-external-module-vals ((module (eql 'multiplayer)) modulepref vals) (set-pref modulepref :multiplayer-options vals))
+(defmethod set-external-module-path ((module (eql 'multiplayer)) modulepref path) 
+  (set-pref modulepref :multiplayer-path path))
+
+(defun def-multiplayer-options () '(7071 7072 "127.0.0.1"))
+
+(defmethod get-external-def-vals ((module (eql 'multiplayer))) 
+    (list :multiplayer-path (when *multiplayer-path* (probe-file *multiplayer-path*))
+          :multiplayer-options (def-multiplayer-options)))
+
+(defmethod save-external-prefs ((module (eql 'multiplayer))) 
+  `(:multiplayer-path ,(om-save-pathname *multiplayer-path*) 
+    :multiplayer-options (list ,*multiplayer-out-port* ,*multiplayer-in-port* ,*multiplayer-host*)))
+
+
+(defmethod put-external-preferences ((module (eql 'microplayer)) moduleprefs)
+  (let ((list-prefs (get-pref moduleprefs :multiplayer-options)))
+    (when list-prefs 
+      (setf *multiplayer-out-port* (nth 0 list-prefs))
+      (setf *multiplayer-in-port* (nth 1 list-prefs))
+      (setf *multiplayer-host* (nth 2 list-prefs))
+      )
+    (when (get-pref moduleprefs :multiplayer-path)
+      (setf *multiplayer-path* (find-true-external (get-pref moduleprefs :multiplayer-path))))
+    ))
+
+(put-external-preferences 'multiplayer (find-pref-module :externals))
+
+
+
+(defmethod show-external-prefs-dialog ((module (eql 'multiplayer)) prefvals)
+
+  (let* ((rep-list (copy-list prefvals))
+         (dialog (om-make-window 'om-dialog
+                                 :window-title "MultiPlayer Options"
+                                 :size (om-make-point 360 220)
+                                 :position :centered
+                                 :resizable nil :maximize nil :close nil))
+         (i 10) initem outitem hostitem)
+    
+    (om-add-subviews dialog
+                     
+                     (om-make-dialog-item 'om-static-text (om-make-point 10 i) (om-make-point 250 24) "MultiPlayer settings" :font *om-default-font2b*)
+
+                     (om-make-dialog-item 'om-static-text (om-make-point 10 (incf i 35)) (om-make-point 150 24) "UDP Ports" :font *om-default-font2*);
+
+                     (om-make-dialog-item 'om-static-text (om-make-point 120 i) (om-make-point 150 20) "OM Out" :font *om-default-font2*)
+                     
+                     (setf outitem (om-make-dialog-item 'om-editable-text (om-make-point 190 i) (om-make-point 42 13)
+                                          (format nil "~D" (nth 0 prefvals))
+                                          :font *om-default-font1*))
+                     
+                     (om-make-dialog-item 'om-static-text (om-make-point 120 (incf i 25)) (om-make-point 150 24) "OM In" :font *om-default-font2*)
+                     
+                     (setf initem (om-make-dialog-item 'om-editable-text (om-make-point 190 i) (om-make-point 42 13)
+                                          (format nil "~D" (nth 1 prefvals))
+                                          :font *om-default-font1*))
+                     
+                     (om-make-dialog-item 'om-static-text (om-make-point 10 (incf i 35)) (om-make-point 150 24) 
+                                          "MultiPlayer Host " :font *om-default-font2*)
+                     
+                     (setf hostitem (om-make-dialog-item 'om-editable-text (om-make-point 165 i) 
+                                                         (om-make-point 100 13)
+                                                         (nth 2 prefvals) 
+                                                         :font *om-default-font1*))
+
+
+
+
+      
+      ;;; boutons
+      (om-make-dialog-item 'om-button (om-make-point 15 (incf i 55)) (om-make-point 90 20) "Restore"
+                           :di-action (om-dialog-item-act item
+                                        (om-set-dialog-item-text outitem (number-to-string (nth 0 (def-multiplayer-options))))
+                                        (om-set-dialog-item-text initem (number-to-string (nth 1 (def-multiplayer-options))))
+                                        (om-set-dialog-item-text hostitem (number-to-string (nth 2 (def-multiplayer-options))))
+                                        ))
+      
+      (om-make-dialog-item 'om-button (om-make-point 160 i) (om-make-point 90 20) "Cancel"
+                           :di-action (om-dialog-item-act item
+                                        (om-return-from-modal-dialog dialog nil)))
+      
+      (om-make-dialog-item 'om-button (om-make-point 250 i) (om-make-point 90 20) "OK"
+                           :di-action (om-dialog-item-act item
+                                        (let* ((argerror nil)
+                                               (intxt (om-dialog-item-text initem)) 
+                                               (in (and (not (string= "" intxt)) (read-from-string intxt)))
+                                               (outtxt (om-dialog-item-text outitem)) 
+                                               (out (and (not (string= "" outtxt)) (read-from-string outtxt))))
+                                         
+                                          (if (and (integerp in)
+                                                   (>= in 0)
+                                                   (integerp out)
+                                                   (>= out 0)
+                                                   (not (= in out)))
+                                              (setf (nth 0 rep-list) out
+                                                    (nth 1 rep-list) in)
+                                            (setf argerror t))
+                                          
+                                          (if (not (string= "" (om-dialog-item-text hostitem)))
+                                              (setf (nth 2 rep-list) (om-dialog-item-text hostitem))
+                                            (setf argerror t))
+                                          
+                                          (if argerror
+                                              (om-message-dialog (format nil "Error in a MultiPlayer option!~%Preference values could not be set."))
+                                            (om-return-from-modal-dialog dialog rep-list))
+                                          ))
+                           :default-button t :focus t)
+      )
+    (om-modal-dialog dialog)))
+
 
 
 
--- OPENMUSIC/code/projects/06-omsounds/multi/sound-players.lisp
+++ OPENMUSIC/code/projects/06-omsounds/multi/sound-players.lisp
@@ -2,81 +2,10 @@
 (in-package :om)
 
 
-(defmethod get-default-score-params ((self sound))
-  (pairlis '(approx fontsize staff cmnpref deltapict outport inport player
-             zoom notechancolor? grillestep mode winsize winpos score-mode obj-mode show-stems scale) 
-           (list *global-midi-approx* *music-fontsize* *default-satff* (make-instance 'edition-values) (om-make-point 0 0) 
-                 nil *InMidiPort* :libaudiostream
-                 1 nil 1000 0 (om-make-point 370 280) (om-make-point 400 20) 0 1 t nil)))
-
-;(defmethod select-player-for-value ((value sound) box player out)
-;  (let ((rep (select-audio-player-dialog box player out)))
-;    (player-special-action (car rep))
-;    rep))
-
-;(defmethod audio-player-name ((self t)) "XXX")
-;(defmethod audio-player-desc ((self t)) "xxx")
-;(defmethod player-special-action ((self t)) nil)
 
 
-(defmethod audio-record-start ((self t)) (om-beep-msg "This player has no audio recording feature"))
-(defmethod audio-record-stop ((self t)) (om-beep))
-
-  
-
-(defmethod player-name ((self (eql :libaudiostream))) "LibAudioStream")
-(defmethod player-desc ((self (eql :libaudiostream))) "internal OM Player")
-
-(defmethod player-name ((self (eql :multiplayer))) "MultiPlayer")
-(defmethod player-desc ((self (eql :multiplayer))) "external Max/OSC player")
-(defmethod player-special-action ((self (eql :multiplayer))) (launch-multiplayer-app))
 
 
-#|
-(defun select-audio-player-dialog (self &optional (player nil) (out nil))
-  (let* ((pane-h (+ 20 (* (length *audio-players*) 25)))
-         (dialog (om-make-window 'om-dialog
-                                :window-title (string+ "Player Settings for " (name self))
-                                :position :centered
-                                :size (om-make-point 350 (+ 120 pane-h))
-                                :maximize nil :resizable nil
-                                :font *om-default-font4*
-                                :bg-color (om-make-color 0.623 0.623 0.623)))
-        (pane (om-make-view 'om-view :bg-color *om-white-color*
-                            :position (om-make-point 10 40) 
-                            :size (om-make-point 320 pane-h)))
-        (i 10) player-items)
-    (setf player-items
-         (loop for pl in *audio-players* collect
-               (let ((item (om-make-dialog-item 'om-radio-button (om-make-point 10 i)
-                                                (om-make-point 260 20) 
-                                                (string+ (audio-player-name pl) " (" (audio-player-desc pl) ")")
-                                                :checked-p (equal pl player)
-                                                :di-action (om-dialog-item-act item t))))
-                 (om-add-subviews pane item)
-                 (incf i 25)
-                 item)))
 
-      (om-add-subviews dialog
-                       (om-make-dialog-item 'om-static-text (om-make-point 10 10) (om-make-point 300 20) 
-                                            (string+ "Select a player for " (name self) " :")
-                                            :font *controls-font*)
-                       pane
-                       
-                       (om-make-dialog-item 'om-button (om-make-point 165 (+ 60 pane-h))
-                                            (om-make-point 80 24) "Cancel" 
-                                            :di-action (om-dialog-item-act item
-                                                         (declare (ignore item))
-                                                         (om-return-from-modal-dialog dialog nil)))
-                       (om-make-dialog-item 'om-button (om-make-point 250 (+ 60 pane-h))
-                                            (om-make-point 80 24) "OK" 
-                                            :di-action (om-dialog-item-act item
-                                                         (declare (ignore item))
-                                                         (om-return-from-modal-dialog dialog 
-                                                                                      (let ((p (position-if 'om-checked-p player-items)))
-                                                                                        (and p (list (nth p *audio-players*)))))
-                                                         )
-                                            :default-button t))
-      (om-modal-dialog dialog)))
 
-|#
+
