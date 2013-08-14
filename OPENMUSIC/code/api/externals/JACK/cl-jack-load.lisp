dff3c1b2b9bd6bd6f9c14e096c7b68e332919e26
andersvi <andersvi@bccda38a-22f4-47c8-a5a1-d3050c704649>
2013-08-14 11:52:20 +0000

    linux: Fluid various moved inside own dir

--- OPENMUSIC/code/api/externals/JACK/cl-jack-load.lisp
+++ OPENMUSIC/code/api/externals/JACK/cl-jack-load.lisp
@@ -5,7 +5,7 @@
   (compile-file-if-needed file)
   (load file))
 
-(setf cl-jack-files '("cl-jack" "cl-jack-midi" "cl-jack-audio" "fluidsynth"))
+(setf cl-jack-files '("cl-jack" "cl-jack-midi" "cl-jack-audio"))
 
 (dolist (file cl-jack-files)
   (compile?-and-load (make-pathname :directory (pathname-directory *load-pathname*) :name file)))
