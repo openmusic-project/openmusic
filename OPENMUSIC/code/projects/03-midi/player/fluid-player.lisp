7317c0c3cc80e9d896e382ffdb47dc0a23d3ab6a
andersvi <andersvi@bccda38a-22f4-47c8-a5a1-d3050c704649>
2013-09-17 14:10:49 +0000

    moved (midi) players for jack-midi and fluidsynth inside 03-midi/player/* - second attempt...

---
+++ OPENMUSIC/code/projects/03-midi/player/fluid-player.lisp
@@ -0,0 +1,115 @@
+;;; ===========================================================================
+;;; FluidSynth Player class for OM.
+;;; 
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU Lesser General Public License as published by
+;;; the Free Software Foundation; either version 2.1 of the License, or
+;;; (at your option) any later version.
+;;;   
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Lesser General Public License for more details.
+;;;   
+;;; You should have received a copy of the GNU Lesser General Public License
+;;; along with this program; if not, write to the Free Software 
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;;; 
+;;; Author: Anders Vinjar
+
+(in-package :om)
+
+;;; fluidsynth event player
+
+(defmethod player-name ((player (eql :fluidsynth))) "Fluidsynth event player")   
+(defmethod player-desc ((player (eql :fluidsynth))) "(default)")   
+(defmethod player-special-action ((player (eql :fluidsynth))) nil)  
+(defmethod player-params ((player (eql :fluidsynth))) nil)   
+(defmethod player-type ((player (eql :fluidsynth))) :midi)   
+
+(progn
+  (pushnew :fluidsynth *all-players*)
+  (enable-player :fluidsynth))
+
+;; setup fluidsynth as optional player for relevant classes
+
+(add-player-for-object simple-score-element :fluidsynth)
+(add-player-for-object score-element :fluidsynth)
+
+
+;; container is already defined, so fine to specialize here:
+
+(defmethod prepare-to-play ((engine (eql :fluidsynth)) (player omplayer) (object container) at interval)
+  (let ((note-in-interval? (interval-intersec interval (list at (+ at (real-dur object))))))
+    (when (or (not interval) note-in-interval?)
+      (mapc #'(lambda (sub)
+		(prepare-to-play engine player sub (+ at (offset->ms sub)) interval))
+	    (inside object)))))
+
+(defmethod prepare-to-play ((engine (eql :fluidsynth)) (player omplayer) object at interval)
+  (call-next-method))
+
+(defvar *fluidplayer-synth* cl-fluidsynth::*fluidsynth*)
+
+(defun play-note (note)
+  (let ((chan (1- (chan note)))		;chans = 1-16
+	(key (floor (midic note) 100))
+	(dur (/ (dur note) 1000))
+	(vel (vel note)))
+    (mp:process-run-function "play fluid note" ()
+			     #'(lambda ()
+				 (cl-fluidsynth::fluid_synth_noteon *fluidplayer-synth* chan key vel)
+				 (sleep dur)
+				 (cl-fluidsynth::fluid_synth_noteoff *fluidplayer-synth* chan key)))))
+
+(defmethod player-play-object ((engine (eql :fluidsynth)) (object note) &key interval)
+  (play-note object))
+
+;; various specializing to play midifile and midievents with fluidsynth:
+
+(defmethod inside ((self midifile))
+  (oa::midi-seq-events (fileseq self)))
+
+;;; FIXME; get proper offset
+(defmethod offset->ms ((self oa::cl-midievent) &optional grandparent)
+  (oa::event-date self))
+
+(defmethod prepare-to-play ((engine (eql :fluidsynth)) (player omplayer) (object midifile) at interval)
+  (let ((note-in-interval? (interval-intersec interval (list at (+ at (real-dur object))))))
+    (when (or (not interval) note-in-interval?)
+      (mapc #'(lambda (sub)
+		(prepare-to-play engine player sub (+ at (offset->ms sub)) interval))
+	    (inside object)))))
+
+;; play event using OMs own scheduler
+(defun play-cl-midievent (event)
+  (when (= (oa::event-type event) (oa::om-midi-get-num-from-type "Note"))
+    (let ((chan (oa::event-chan event))	
+	  (key (oa::event-pitch event))
+	  (dur (/ (oa::event-dur event) 1000.0))
+	  (vel (oa::event-velocity event)))
+      (mp:process-run-function "play fluid event" ()
+			       #'(lambda ()
+				   (cl-fluidsynth::fluid_synth_noteon *fluidplayer-synth* chan key vel)
+				   (sleep dur)
+				   (cl-fluidsynth::fluid_synth_noteoff *fluidplayer-synth* chan key))))))
+
+(defmethod player-play-object ((engine (eql :fluidsynth)) (object oa::cl-midievent) &key interval)
+  (when (or (not interval) (point-in-interval (offset->ms object) interval))
+    (play-cl-midievent object)))
+
+;; 'edition-params is looked up by new-player, and seems to be
+;; defaulted to :midishare somewhere:
+
+(defmethod get-edit-param ((box ommidifilebox) (param (eql 'player))) 
+  :fluidsynth)
+
+
+;; (defmethod get-player-engine ((self editorview)) (get-edit-param self 'player))
+;; (defmethod get-player-engine ((self play-editor-mixin)) t)
+
+;; (defmethod set-edit-param ((self EditorView) param val) 
+;;    (if (and (editor-edit-params self) (assoc param (editor-edit-params self)))
+;;      (rplacd (assoc param (editor-edit-params self)) val)
+;;      (when (edition-params (ref self))
+;;        (push (cons param val) (edition-params (ref self))))))
