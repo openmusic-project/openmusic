c16ba5e36c9f9c96220021c870fd401be0b41671
bresson <bresson@bccda38a-22f4-47c8-a5a1-d3050c704649>
2013-10-02 14:09:19 +0000

    check merge in commit 540 [cont.]

--- OPENMUSIC/code/api/externals/Audio/load-audio.lisp
+++ OPENMUSIC/code/api/externals/Audio/load-audio.lisp
@@ -8,8 +8,6 @@
 
 (compile&load (make-pathname :directory (pathname-directory *load-pathname*) :name "las-audio-player"))                                      
 
-
-(compile&load (make-pathname :directory (append (pathname-directory *load-pathname*) (list "LibAudioStream")) :name "faust-api"))
 (compile&load (make-pathname :directory (append (pathname-directory *load-pathname*) (list "libsndfile")) :name "libsndfile"))
 
 (compile&load (make-pathname :directory  (pathname-directory *load-pathname*) :name "audio-api"))
