ebce19b5ea1cb2b6ebedf54dee09592299edea6c
andersvi <andersvi@bccda38a-22f4-47c8-a5a1-d3050c704649>
2013-10-23 14:04:17 +0000

    header in midi-types.lisp

--- OPENMUSIC/code/api/externals/MIDI/CL-MIDI/midi-types.lisp
+++ OPENMUSIC/code/api/externals/MIDI/CL-MIDI/midi-types.lisp
@@ -1,3 +1,26 @@
+;;===========================================================================
+;;; midi-types.lisp
+;;; Common Lisp Midi API - based on ms:: versions found in midi-api.lisp
+;;; 
+;;; This program is free software;;;  you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation;;;  either version 2
+;;; of the License, or (at your option) any later version.
+;;; 
+;;; See file LICENSE for further informations on licensing terms.
+;;; 
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY;;;  without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;; 
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program;;;  if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+;;; 
+;;; Author: Anders Vinjar
+
+
 (defpackage :lispmidi (:nicknames :ml))
 (in-package :ml)
 
