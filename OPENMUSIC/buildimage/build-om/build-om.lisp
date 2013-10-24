f6dced3860e4cf5b742b9a6befa4de051431d7de
andersvi <andersvi@bccda38a-22f4-47c8-a5a1-d3050c704649>
2013-10-24 19:33:06 +0000

    typo

--- OPENMUSIC/buildimage/build-om/build-om.lisp
+++ OPENMUSIC/buildimage/build-om/build-om.lisp
@@ -11,7 +11,7 @@
 
 ;;;=======================================
 (defparameter *app-name* "OM")
-(defparameter *version* 6.07000
+(defparameter *version* 6.070002)
 (defparameter *beta-release* t)
 (defparameter *version-str* "")
 (defparameter *release-language* :en)
