2e0b2dab89e0f8488e6a0c175dc0a9bddcba5530
andersvi <andersvi@bccda38a-22f4-47c8-a5a1-d3050c704649>
2014-01-07 10:38:34 +0000

    6.7.1 beta 3 build

--- OPENMUSIC/buildimage/build-om/build-om.lisp
+++ OPENMUSIC/buildimage/build-om/build-om.lisp
@@ -11,7 +11,7 @@
 
 ;;;=======================================
 (defparameter *app-name* "OM")
-(defparameter *version* 6.070101)
+(defparameter *version* 6.070103)
 (defparameter *beta-release* t)
 (defparameter *version-str* "")
 (defparameter *release-language* :en)
