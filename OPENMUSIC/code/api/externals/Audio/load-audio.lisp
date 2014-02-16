bc1201bf7cd189c616066384e9fb2efe219f9c12
bresson <bresson@bccda38a-22f4-47c8-a5a1-d3050c704649>
2014-02-16 00:42:19 +0000

    updated audio tutorials

--- OPENMUSIC/code/api/externals/Audio/load-audio.lisp
+++ OPENMUSIC/code/api/externals/Audio/load-audio.lisp
@@ -7,14 +7,14 @@
   (:nicknames "AU")
   (:use cl-user common-lisp))
 
-(compile&load (make-pathname :directory (append (pathname-directory *load-pathname*) (list "LibAudioStream")) :name "LibAudioStream"))
-(compile&load (make-pathname :directory (pathname-directory *load-pathname*) :name "las-audio-player"))                                      
-
 (compile&load (make-pathname :directory (append (pathname-directory *load-pathname*) (list "libsndfile")) :name "libsndfile"))
 (compile&load (make-pathname :directory (append (pathname-directory *load-pathname*) (list "libsamplerate")) :name "libsamplerate"))
 
 (compile&load (make-pathname :directory (append (pathname-directory *load-pathname*) (list "libsndfile")) :name "sndfile-api"))
 (compile&load (make-pathname :directory  (pathname-directory *load-pathname*) :name "audio-api"))
+
+(compile&load (make-pathname :directory (append (pathname-directory *load-pathname*) (list "LibAudioStream")) :name "LibAudioStream"))
+(compile&load (make-pathname :directory (append (pathname-directory *load-pathname*) (list "LibAudioStream")) :name "las-audio-player"))                                      
 
 (push :om-audio *features*)
 
