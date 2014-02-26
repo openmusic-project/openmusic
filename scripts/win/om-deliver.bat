fd85126d2d12204e1ec38ed5cfd80da86f043d61
bresson <bresson@bccda38a-22f4-47c8-a5a1-d3050c704649>
2014-02-26 09:09:48 +0000

    build scripts for mac/windows

---
+++ scripts/win/om-deliver.bat
@@ -0,0 +1,5 @@
+cd ..\..\OPENMUSIC\buildimage\build-om\
+
+"C:\\Program Files (x86)\\LispWorks\\lispworks-6-1-0-x86-win32.exe" -init deliver.lisp
+
+cd ..\..\..\scripts\win\
---
+++ scripts/win/om-pack.bat
@@ -0,0 +1,5 @@
+cd ..\..\OPENMUSIC\buildimage\build-om\
+
+"C:\\Program Files (x86)\\LispWorks\\lispworks-6-1-0-x86-win32.exe" -init pack-win.lisp
+
+cd ..\..\..\scripts\win\
