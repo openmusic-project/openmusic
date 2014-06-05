0bf798c825998b3210f72c10410749a12a305486
Anders Vinjar <andersvi@notam02.no>
2014-06-05 14:42:24 +0200

    linux: (portmidi) remove hardcoded path to .so

--- OPENMUSIC/code/api/externals/MIDI/PortMidi/portmidi.lisp
+++ OPENMUSIC/code/api/externals/MIDI/PortMidi/portmidi.lisp
@@ -31,19 +31,25 @@
   #+macosx
   "/usr/lin/libportmidi.dylib"
   #+linux
-  "/usr/lib/libportmidi.so")
+  "libportmidi.so")
 
 (defvar *libportmidi* nil)
 
-(defun load-portmidi-lib ()
-  (print (concatenate 'string "Loading PortMIDI library: " (namestring *libportmidi-pathname*)))
-  (if (probe-file *libportmidi-pathname*)
-      (setf *libportmidi* (fli:register-module "PortMidi" 
-                                               :real-name (namestring *libportmidi-pathname*)
-                                               :connection-style :immediate)
-            )
-    (print (format nil "Library PortMIDI not found!! [~A]" (namestring *libportmidi-pathname*)))
-    ))
+#-linux (defun load-portmidi-lib ()
+	  (print (concatenate 'string "Loading PortMIDI library: " (namestring *libportmidi-pathname*)))
+	  (if (probe-file *libportmidi-pathname*)
+	      (setf *libportmidi* (fli:register-module "PortMidi" 
+						       :real-name (namestring *libportmidi-pathname*)
+						       :connection-style :immediate)
+		    )
+	      (print (format nil "Library PortMIDI not found!! [~A]" (namestring *libportmidi-pathname*)))
+	      ))
+
+#+linux (defun load-portmidi-lib ()
+	  (print (concatenate 'string "Loading PortMIDI library: " (namestring *libportmidi-pathname*)))
+	  (setf *libportmidi* (fli:register-module (namestring *libportmidi-pathname*) 
+						   :connection-style :immediate)))
+
 
 ; (load-portmidi-lib)
 
