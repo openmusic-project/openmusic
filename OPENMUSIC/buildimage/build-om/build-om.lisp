96482f356290390ebc3607eb469ad0b66e9c3477
Jean Bresson <bresson@m2375.ircam.fr>
2014-09-23 15:34:51 +0200

    beta 2

--- OPENMUSIC/buildimage/build-om/build-om.lisp
+++ OPENMUSIC/buildimage/build-om/build-om.lisp
@@ -11,7 +11,7 @@
 
 ;;;=======================================
 (defparameter *app-name* "OM")
-(defparameter *version* 6.090001)
+(defparameter *version* 6.090002)
 (defparameter *beta-release* t)
 (defparameter *version-str* "")
 (defparameter *release-language* :en)
