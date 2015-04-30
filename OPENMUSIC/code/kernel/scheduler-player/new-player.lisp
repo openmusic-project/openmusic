c9e6a39f757c6d0947a4a7275cf99fef9eb5149f
Jean Bresson <bresson@ircam.fr>
2015-04-30 22:19:15 +0200

    sound box edit params

--- OPENMUSIC/code/kernel/scheduler-player/new-player.lisp
+++ OPENMUSIC/code/kernel/scheduler-player/new-player.lisp
@@ -325,7 +325,7 @@
 (defmethod stop-boxes ((boxlist list))
   (mapcar #'(lambda (box)
               (when (play-obj? (value box))
-                (player-stop (get-param box 'player) (list (value box)))
+                (player-stop (get-edit-param box 'player) (list (value box)))
                 (setf (play-state box) nil)
                 (setf *play-boxes* (remove box *play-boxes*))
                 ))
@@ -339,8 +339,8 @@
 
 
 (defmethod additional-player-params ((self omboxeditcall))
-  (list :port (get-param self 'outport)
-        :approx (get-param self 'approx)))
+  (list :port (get-edit-param self 'outport)
+        :approx (get-edit-param self 'approx)))
 
 
 ;;;=================================
