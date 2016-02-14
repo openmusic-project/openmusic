c8e15c062fa4d5e03da85b0924823f85d3eddebb
Jean Bresson <bresson@ircam.fr>
2016-02-13 18:49:14 -0800

    de-fix set-layout

--- OPENMUSIC/code/api/om-LW/view.lisp
+++ OPENMUSIC/code/api/om-LW/view.lisp
@@ -85,7 +85,7 @@
         (remove-duplicates (remove nil (cons (main-pinboard-object view)
                                              (append (vsubviews view) (item-subviews view))))))
   ;; this fixed the problem with dialog item box display on windows...
-  #-linux (setf (pinboard-pane-position view) (values (vx view) (vy view)))
+  #+windows (setf (pinboard-pane-position view) (values (vx view) (vy view)))
   )
 
 (defmethod set-layout ((view t)) nil)
