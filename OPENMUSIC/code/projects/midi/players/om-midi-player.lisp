83f6d75b6bfd0b1350f11aa843a53ec094c9e39d
Anders Vinjar <anders.vinjar@bek.no>
2016-12-07 17:38:14 +0100

    microtune: tune only adjacent chans for various approxes: (2 4 8)

--- OPENMUSIC/code/projects/midi/players/om-midi-player.lisp
+++ OPENMUSIC/code/projects/midi/players/om-midi-player.lisp
@@ -16,6 +16,7 @@
     
 (defmethod prepare-to-play ((engine (eql :midi-player)) (player omplayer) object at interval params)
   ;(print (format nil "~s" params))
+  
   (let ((approx (if (find :approx params)
                     (nth (1+ (position :approx params)) params)
                   (if (caller player) (get-edit-param (caller player) 'approx))))
@@ -37,7 +38,8 @@
             
             
             (append (and (and *midi-microplay* approx (find approx '(4 8) :test '=))
-                         (microplay-events at (get-obj-dur object) port))
+			 (let ((chan-offset (lchan object)))
+			   (microplay-events approx at (get-obj-dur object) port chan-offset)))
                     (remove nil (flat (PrepareToPlay :midi object at :interval interval :approx approx :port port)))
                     )
             )
@@ -68,23 +70,21 @@
 
 ;;; PLAY (NOW) 
 (defmethod player-play-object ((engine (eql :midi-player)) (object om-midi::midi-evt) &key interval params)
-  ;(print (format nil "~A : play ~A - ~A" engine object interval))
-  ;(print object)
-  (cond 
-   ((or (equal (om-midi::midi-evt-type object) :keyOff)
-        (and (equal (om-midi::midi-evt-type object) :keyOn) (= 0 (cadr (om-midi::midi-evt-fields object)))))
-    (setf (nth (1- (om-midi::midi-evt-chan object)) *key-ons*) 
-          (delete (list (om-midi::midi-evt-port object) (car (om-midi::midi-evt-fields object)))
-                  (nth (1- (om-midi::midi-evt-chan object)) *key-ons*)
-                  :test 'equal)))
-   ((equal (om-midi::midi-evt-type object) :keyOn)
-    (pushnew (list (om-midi::midi-evt-port object) (car (om-midi::midi-evt-fields object))) (nth (1- (om-midi::midi-evt-chan object)) *key-ons*) :test 'equal))
-   
-   )
-  ;(print *key-ons*)
-  (midi-send-evt object)
-  )
-
+  ;;(print (format nil "~A : play ~A - ~A" engine object interval))
+  ;;(print object)
+  (let ((key-index (om-midi::midi-evt-chan object)))
+    (cond 
+      ((or (equal (om-midi::midi-evt-type object) :keyOff)
+	   (and (equal (om-midi::midi-evt-type object) :keyOn) (= 0 (cadr (om-midi::midi-evt-fields object)))))
+       (setf (nth key-index *key-ons*) 
+	     (delete (list (om-midi::midi-evt-port object) (car (om-midi::midi-evt-fields object)))
+		     (nth key-index *key-ons*)
+		     :test 'equal)))
+      ((equal (om-midi::midi-evt-type object) :keyOn)
+       (pushnew (list (om-midi::midi-evt-port object) (car (om-midi::midi-evt-fields object))) (nth key-index *key-ons*) :test 'equal)))
+    
+    ;;(print *key-ons*)
+    (midi-send-evt object)))
 
 ;;;==============================
 ;;; MICROTONALITE
@@ -128,16 +128,17 @@
   (funcall send-fun (make-pitchwheel-event 0 4 p 11264))
   ))
 
-(defun microplay-events (at dur port)
-  ;;; make or send ... ?
-  (let ((port (or port *def-midi-out*)))
-    (list (make-pitchwheel-event at 1 port 8192) 
-          (make-pitchwheel-event at 2 port 9216) 
-          (make-pitchwheel-event at 3 port 10240) 
-          (make-pitchwheel-event at 4 port 11264) 
-          (make-pitchwheel-event (+ at dur) 1 port 8192) 
-          (make-pitchwheel-event (+ at dur) 2 port 8192) 
-          (make-pitchwheel-event (+ at dur) 3 port 8192) 
-          (make-pitchwheel-event (+ at dur) 4 port 8192))))
+(defun microplay-events (approx at dur port chan-offset)
+  (let ((port (or port *def-midi-out*))
+	(offset (car (remove-duplicates (flat chan-offset)))))
+    (append (loop
+	       repeat (/ approx 2)
+	       for bend from 8192 by (/ 8192 approx)
+	       for chan from offset
+	       collect (make-pitchwheel-event at chan port bend))
+	    (loop
+	       repeat (/ approx 2)
+	       for chan from offset
+	       collect (make-pitchwheel-event (+ at dur) chan port 8192)))))
 
 
