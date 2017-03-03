openmusic-project/openmusic

commit f6ea4a0e75fd2b817d02e6071c85d5f7c9d0fd7b

work on menus (adapt macOS Sierra LW bug)

OPENMUSIC/code/kernel/ommodele/omframes/containereditors/editorwindow.lisp
@@ -142,8 +142,6 @@
 (defmethod om-window-class-menubar ((self EditorWindow))
      (get-menubar (editor self)))
 
-
- 
 (defmethod make-editor-window ((class t) object name ref &key 
                                winsize winpos (close-p t) (winshow t) (resize t) (retain-scroll nil)
                                (wintype nil))
@@ -158,7 +156,7 @@
                                :close close-p
                                :resizable resize
                                :maximize resize
-                               :window-show nil
+                               :window-show winshow
                                :toolbox (member :toolbox wintype) 
                                :size sizewin
                                :obj (editor-object-from-value object)))
@@ -172,7 +170,7 @@
           )
       (setf (editor win) editor)
       (om-add-menu-to-win win)  
-      (when winshow (om-select-window win))
+      (om-select-window win)
       (om-set-view-size editor (om-interior-size win))
       win))
 

OPENMUSIC/code/api/om-LW/menu.lisp
@@ -223,12 +223,12 @@
 
 (defun om-add-menu-to-win (window) 
  (let ((menubar (remove nil (append (default-app-menubar window) (om-window-class-menubar window)))))
-   (capi::execute-with-interface window
-                                 #'(lambda () (setf (capi::interface-menu-bar-items window) menubar)
-                                     ))))
+   (capi::execute-with-interface 
+    window
+    #'(lambda () (setf (capi::interface-menu-bar-items window) menubar)
+        ))))
 
 
-  
 ;;;;===================
 ;;;; POP UP / CONTEXT MENU
 ;;;;===================

OPENMUSIC/code/api/om-LW/window.lisp
@@ -329,60 +329,64 @@
 
 (defmethod correct-win-h ((win t)) nil)
 
-(defmacro om-make-window (class &rest attributes 
+(defun om-make-window (class &rest attributes 
 			  &key (position (om-make-point 200 200)) (size (om-make-point 500 400))
 			  name window-title owner (font *om-default-font2*)  (bg-color *om-white-color*)
 			  (window-show t) subviews (resizable t) (close t) (minimize t) (maximize t) (topmost nil) (toolbox nil)
+                          menu-items
 			  &allow-other-keys)  
-  `(let* ((pos ,position)
-            (size ,size)
-            (winparent (or ,owner (capi:convert-to-screen)))
-            (winname (or ,name (string (gensym))))
-            (title (or ,window-title ,name "OM Window"))
-            (w (om-point-h size))
-            (h (om-point-v size))
-          (x (if (equal pos :centered) (round (- (* (capi::screen-width winparent) 0.5) (* w 0.5))) (om-point-h pos)))
-          (y (if (equal pos :centered) (round (- (* (capi::screen-height winparent) 0.5) (* h 0.5))) (om-point-v pos)))
-          (style (append (get-window-styles-from-class ,class) 
-                         (when ,(not minimize) (list :never-iconic))
-                         (when ,topmost (list :always-on-top))
-                         (when ,toolbox (list :toolbox))
-                         (list :no-character-palette)))
-          layout thewin)
+  (let* ((pos position)
+         (size size)
+         (winparent (or owner (capi:convert-to-screen)))
+         (winname (or name (string (gensym))))
+         (title (or window-title name "OM Window"))
+         (w (om-point-h size))
+         (h (om-point-v size))
+         (x (if (equal pos :centered) (round (- (* (capi::screen-width winparent) 0.5) (* w 0.5))) (om-point-h pos)))
+         (y (if (equal pos :centered) (round (- (* (capi::screen-height winparent) 0.5) (* h 0.5))) (om-point-v pos)))
+         (style (append (get-window-styles-from-class class) 
+                        (when (not minimize) (list :never-iconic))
+                        (when topmost (list :always-on-top))
+                        (when toolbox (list :toolbox))
+                        (list :no-character-palette)))
+         layout thewin)
      ;(if (> (+ x w) (capi::screen-width (capi:convert-to-screen nil))) 
      ;    (setf w (- (capi::screen-width (capi:convert-to-screen nil)) x)))
      ;(if (> (+ y h) (capi::screen-height (capi:convert-to-screen nil))) 
      ;    (setf h (- (capi::screen-height (capi:convert-to-screen nil)) y)))
      ;(print (list "SIZE FOR MAKE INSTANCE IS" size))
-     (setf thewin (make-instance ,class
-                                 :title title :name winname
-                                 :x x :y y 
-                                 :width w :height h
-                                 ;:external-min-width w :external-min-height h
-                                 ;;; this works for Windows (on Mac : :width / :height ==> a tester)
-                                 :parent winparent
-                                 :internal-border nil :external-border nil
-                                 :display-state (if ,window-show :normal :hidden)
-                                 ;:window-styles '(:internal-borderless)
-                                 ;:external-min-height 50 :external-min-width 50
-                                 :no-character-palette t
-                                 :menu-bar-items nil
-                                 :window-styles style
-                                 :font ,font
-                                 :resizable ,resizable
-                                 ;;; OM-GRAPHIC-OBJECT SLOTS
-                                 :vx x :vy y :vw w :vh h
-                                 :allow-other-keys t
-                                 ,.attributes
-                                 ))
+    (setf thewin (apply 'make-instance 
+                        (cons class
+                              (append 
+                               (list :title title :name winname
+                                     :x x :y y 
+                                     :width w :height h
+                                     ;:external-min-width w :external-min-height h
+                                     ;;; this works for Windows (on Mac : :width / :height ==> a tester)
+                                     :parent winparent
+                                     :internal-border nil :external-border nil
+                                     :display-state (if window-show :normal :hidden)
+                                     ;:window-styles '(:internal-borderless)
+                                     ;:external-min-height 50 :external-min-width 50
+                                     :no-character-palette t
+                                     ;:menu-bar-items nil
+                                     :activate-callback #'(lambda (win activate-p) (when activate-p (om-add-menu-to-win win)))
+                                     :window-styles style
+                                     :font font
+                                     :resizable resizable
+                                     ;;; OM-GRAPHIC-OBJECT SLOTS
+                                     :vx x :vy y :vw w :vh h
+                                     :menu-bar-items menu-items
+                                     :allow-other-keys t)
+                               attributes
+                               ))))
      
-     (when (setf layout (make-window-layout thewin ,bg-color))
-       #+cocoa(if (drawable-layout layout) (setf (capi::output-pane-display-callback layout) 'om-draw-contents-callback))
-       (setf (capi::pane-layout thewin) layout)
-       )
-     (when ,subviews (mapc (lambda (sv) (om-add-subviews thewin sv)) ,subviews))
+    (when (setf layout (make-window-layout thewin bg-color))
+      #+cocoa(if (drawable-layout layout) (setf (capi::output-pane-display-callback layout) 'om-draw-contents-callback))
+      (setf (capi::pane-layout thewin) layout))
+     (when subviews (mapc (lambda (sv) (om-add-subviews thewin sv)) subviews))
      (correct-win-h thewin)
-     (when (or (not ,resizable) (window-dialog-p thewin))
+     (when (or (not resizable) (window-dialog-p thewin))
        (set-not-resizable thewin w h))
      (unless (window-dialog-p thewin)
        (internal-display thewin))

commit 531996b2ecbeab36f66a1609dda593f86e491bea

fix correct bug introduced in f6ea4a0 - revert c72dffd
This reverts commit c72dffd925e3a219bf9360dbce10ac2a128152d8.

OPENMUSIC/code/kernel/ommodele/omframes/containereditors/editorwindow.lisp
@@ -170,7 +170,7 @@
           )
       (setf (editor win) editor)
       (om-add-menu-to-win win)  
-      (om-select-window win)
+      (when winshow (om-select-window win))
       #-linux (om-set-view-size editor (om-interior-size win))
       win))
 

OPENMUSIC/code/kernel/ommodele/omframes/simpleframes/iconsframe/iconfinder.lisp
@@ -116,28 +116,27 @@ this slot store a subview containing it subview if exists.#triangle#
 (defmethod add-icon-finder ((self icon-finder) container)
   ;; (unless (equal (om-get-bg-color (nameview self)) *om-transparent-color*)
   ;;  (om-set-part-color (nameview self) :body (om-get-bg-color container)))
-  (progn
-    (om-add-subviews container self)
-    (sleep 0.01)
-    (set-size self container)
-    (if (big-icon-p (editor container))
-	(change-icon-position self (bx self) (by self))
-	(progn
-	  (add-extras-infos self)
-	  (flet ((match (x) (equal (col x) (col self))))
-	    (let* ((controls (get-subframes container))
-		   (pos (position-if #'match controls))
-		   final-list)
-	      (when pos
-		(setf final-list (subseq controls (+ pos 1)))
-		(change-icon-position self (fil self) (col self))
-		(mapc #'(lambda (icon)
-			  (change-icon-position icon (fil icon) (+ (col icon) 1)))
-		      final-list))))
-	  (when (container-p self)
-	    (add-triangle self container))
-	  (om-invalidate-view container)))))
-
+  (om-add-subviews container self)
+  (set-size self container)
+  (if (big-icon-p (editor container))
+      (change-icon-position self (bx self) (by self))
+      (progn
+	(add-extras-infos self)
+	(flet ((match (x) (equal (col x) (col self))))
+	  (let* ((controls (get-subframes container))
+		 (pos (position-if #'match controls))
+		 final-list)
+	    (when pos
+	      (setf final-list (subseq controls (+ pos 1)))
+	      (change-icon-position self (fil self) (col self))
+	      (mapc #'(lambda (icon)
+			(change-icon-position icon (fil icon) (+ (col icon) 1)))
+		    final-list)
+	      )))
+	(when (container-p self)
+	  (add-triangle self container))
+	(om-invalidate-view container)
+      	)))
 
 
 (defun add-triangle (self container)