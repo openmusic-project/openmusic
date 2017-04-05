b2fabe44638e80188490e438b68deaf97b293a8c
Anders Vinjar <anders.vinjar@bek.no>
2017-04-05 15:43:24 +0200

    add optional 'middle-C' (def=3) to note parsers n->mc, mc->n
    (mc->f (n->mc "a4"))   -> 880.0
    (mc->f (n->mc "a4" 4)) -> 440.0

--- OPENMUSIC/code/projects/musicproject/functions/conversions.lisp
+++ OPENMUSIC/code/projects/musicproject/functions/conversions.lisp
@@ -186,7 +186,7 @@ Floating values are allowed for <approx>.
              (apply #'deep-mapcar fun fun1 (cdr list?) args)))))
 
 
-(defun mc->n1 (midic &optional (ascii-note-scale (car *ascii-note-scales*)))
+(defun mc->n1 (midic &optional (ascii-note-scale (car *ascii-note-scales*)) (middle-C 3))
   "Converts <midic> to a string representing a symbolic ascii note."
   (let ((dmidic (/ 1200 (length ascii-note-scale))) note)
     (multiple-value-bind (midic/50 cents) (round midic dmidic)
@@ -194,12 +194,12 @@ Floating values are allowed for <approx>.
         (setq note (nth (/ midic<1200 dmidic) ascii-note-scale))
         (format nil "~A~A~A~A~A"
           (car note) (or (car (cassq (cdr note) *ascii-note-alterations*)) "")
-          (- oct+2 2) (if (> cents 0) "+" "") (if (zerop cents) "" cents) )))))
+          (- oct+2 (- 5 middle-C)) (if (> cents 0) "+" "") (if (zerop cents) "" cents) )))))
 
 ; (mc->n1 6000)
-; (mc->n1 6000 *ascii-note-do-scale*)
+; (mc->n1 6000 *ascii-note-do-scale* 3)
 
-(defun n->mc1 (str &optional (*ascii-note-scale* (car *ascii-note-scales*)))
+(defun n->mc1 (str &optional (*ascii-note-scale* (car *ascii-note-scales*)) (middle-C 3))
   "Converts a string representing a symbolic ascii note to a midic."
   (if (integerp str)
       str
@@ -225,7 +225,7 @@ Floating values are allowed for <approx>.
 	    (incf index (length (second alt))))
 	  ;; octave
 	  (multiple-value-bind (oct i) (parse-integer str :start index :junk-allowed t)
-	    (incf midic (* (+ oct 2) 1200))
+	    (incf midic (* (+ oct (- 5 middle-C)) 1200))
 	    (setq index i))
 	  (unless (= index (length str))
 	    (incf midic (parse-integer str :start index)))
@@ -311,43 +311,46 @@ major third down 6m-1, returns -400 in midicents ."
   (om::deep-mapcar #'symb->int #'symb->int1 ints))
 
 
-(om::defmethod! mc->n ((midics? list))
+(om::defmethod! mc->n ((midics? list) &optional (middle-C 3))
   :initvals '((6000)) 
   :indoc '("pitch or pitch list (midicents)")
   :icon 141
   :doc  "
 Converts <midics> to symbolic (ASCII) note names. 
 
-Symbolic note names follow standard notation with middle c (midicent 6000) being C3. 
+Symbolic note names follow standard notation.
+Middle c (midicent 6000) being C3 by default, can be set to be C4 by the optional input.
 Semitones are labeled with a '#' or a 'b.'  
 Quartertone flats are labeled with a '_', and quartertone sharps with a '+' (ex. C3 a quartertone sharp (midi-cent 6050), would be labeled 'C+3'. 
 Gradations smaller than a quartertone are expressed as the closest  quartertone + or - the remaining cent value (ex. midi-cent 8176 would be expressed as Bb4-24).
 "
  
-  (om::deep-mapcar 'mc->n 'mc->n1 midics?))
+  (om::deep-mapcar 'mc->n #'(lambda (mc) (mc->n1 mc (car *ascii-note-scales*) middle-C)) midics?))
 
-(om::defmethod! mc->n ((midic number))
-  (mc->n1 midic))
+(om::defmethod! mc->n ((midic number) &optional (middle-C 3))
+  (mc->n1 midic (car *ascii-note-scales*) middle-C))
 
-(om::defmethod! n->mc ((strs list))
-  :initvals '(("C3")) 
-  :indoc '("note name or list of note names")
+(om::defmethod! n->mc ((strs list) &optional (middle-C 3))
+  :initvals '(("C3") ) 
+  :indoc '("note name or list of note names" "octave of middle C")
+  :menuins '((1 (("middle-C = 3" 3) ("middle-C = 4" 4))))
   :icon 141
   :doc   "
 Converts <strs> to pitch values in midicents. 
 
-Symbolic note names follow standard notation with middle c (midicent 6000) being C3. 
-Semitones are labeled with a '#' or a 'b.'  
+Symbolic note names follow standard notation. 
+Middle c (midicent 6000) being C3 by default, can be set to be C4 by the optional input.
+Semitones are labeled with a '#' or a 'b.', or a 'f' or a 's', e.g. 'G#3' or 'GS3'.
 Quartertone flats are labeled with a '_', and quartertone sharps with a '+' (ex. C3 a quartertone sharp (midi-cent 6050), would be labeled 'C+3'. 
 Gradations smaller than a quartertone are expressed as the closest  quartertone + or - the remaining cent value (ex. midi-cent 8176 would be expressed as Bb4-24).
 " 
-  (om::deep-mapcar 'n->mc 'n->mc1 strs))
+  (om::deep-mapcar 'n->mc #'(lambda (n) (n->mc1 n (car *ascii-note-scales*) middle-C)) strs))
 
-(om::defmethod! n->mc ((strs string))
-  (car (n->mc (list strs))))
+(om::defmethod! n->mc ((strs string) &optional (middle-C 3))
+  (car (n->mc (list strs) middle-C)))
 
-(om::defmethod! n->mc ((symb symbol))
-  (n->mc (string symb)))
+(om::defmethod! n->mc ((symb symbol) &optional (middle-C 3))
+  (n->mc (string symb) middle-C))
 
 ;;;=======================================
 ;;; TEMPO UTILS
