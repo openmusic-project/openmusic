12b6ff81cde6d648e17d20b06dc7f6b00222f574
Jean Bresson <bresson@ircam.fr>
2017-07-25 13:12:21 +0200

    fix select-tree default value

--- OPENMUSIC/code/projects/musicproject/functions/trees.lisp
+++ OPENMUSIC/code/projects/musicproject/functions/trees.lisp
@@ -513,10 +513,10 @@ Replaces expressed notes in given positions from <places> with rests.
 
 
 (defmethod! select-tree ((tree list) (places list))
-   :initvals '('(? (((4 4) (1 (1 (1 2 1 1)) 1 1)) ((4 4) (1 (1 (1 2 1 1)) -1 1)))) '(0 1))
+   :initvals '((2 (((4 4) (1 (1 (1 2 1 1)) 1 1)) ((4 4) (1 (1 (1 2 1 1)) -1 1)))) (0 1))
    :indoc '("tree" "places")
    :icon 225
-   :doc "selects expressed notes only in given places <places> "
+   :doc "Selects expressed notes only in given places <places> "
    (let* ((pulse-places (arithm-ser 0 (n-pulses tree) 1))
           (positions (x-diff pulse-places places)))
      (filtertree tree positions)))
