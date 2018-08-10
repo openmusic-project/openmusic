b911297082654664ed954437ce8eeebb957ed79e
j-bresson <bresson@ircam.fr>
2018-08-10 17:33:23 +0200

    version => 6.13.2

--- OPENMUSIC/build/build-om.lisp
+++ OPENMUSIC/build/build-om.lisp
@@ -37,7 +37,7 @@
 ;;;=======================================
 (defparameter *app-name* "OM")
 
-(defparameter *version* 6.130100)
+(defparameter *version* 6.130200)
 
 (defparameter *beta-release* nil) 
 (defparameter *version-str* "")
