5bf66e7acf1c1c437bedf29b7157458370278c8f
karimhaddad <karim.haddad.ircam.fr>
2020-02-24 16:12:25 +0100

    incremented verion number

--- OPENMUSIC/build/build-om.lisp
+++ OPENMUSIC/build/build-om.lisp
@@ -37,7 +37,7 @@
 ;;;=======================================
 (defparameter *app-name* "OM")
 
-(defparameter *version* 6.160000)
+(defparameter *version* 6.170000)
 
 (defparameter *beta-release* nil) 
 (defparameter *version-str* "")
