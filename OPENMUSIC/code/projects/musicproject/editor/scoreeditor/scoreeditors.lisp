7d550cc806c8f37a6466aee8ce7fc27137bd8fd5
karimhaddad <karim.haddad@ircam.fr>
2021-02-05 15:10:53 +0100

    shortcut for inspector palette

--- OPENMUSIC/code/projects/musicproject/editor/scoreeditor/scoreeditors.lisp
+++ OPENMUSIC/code/projects/musicproject/editor/scoreeditor/scoreeditors.lisp
@@ -876,6 +876,7 @@
            (#\m (get-tempo-tree self)) 
            (#\M (remove-tempo self))
            (#\i (get-obj-info self))
+           (#\I (get-inspector-win (panel self)))
            (#\c (note-chan-color self))
            (#\n (set-name-to-mus-obj self))
            (#\h (show-help-window (format nil "Commands for ~A Editor" 
