52229c746b329cfbd4335d7faf8b6e41f828cc85
karimhaddad <karim.haddad@ircam.fr>
2022-01-12 14:11:45 +0100

    typos

--- OPENMUSIC/code/projects/musicproject/editor/scoreeditor/scoreeditors.lisp
+++ OPENMUSIC/code/projects/musicproject/editor/scoreeditor/scoreeditors.lisp
@@ -174,15 +174,15 @@
                                             (update-player-controls editor newplayer)))))))
 
   (setf (time-view (title-bar self)) 
-        #-(and cocoa lispwork8)(om-make-dialog-item 'om-static-text 
+        #-(and cocoa lispworks8)(om-make-dialog-item 'om-static-text 
                                                     (om-make-point 600 5) 
                                                     (om-make-point 200 15)
                                                     "")
-        #+(and cocoa lispwork8)(om-make-dialog-item 'om-text-view 
+        #+(and cocoa lispworks8)(om-make-dialog-item 'om-text-view 
                                                     (om-make-point 600 5) 
                                                     (om-make-point 200 15)
                                                     ""
-                                                    :bg-color *control-color*))
+                                                    :bg-color *controls-color*))
 
   (apply 'om-add-subviews (cons (title-bar self) 
                                 (cons (time-view (title-bar self))
