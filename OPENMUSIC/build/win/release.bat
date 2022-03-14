1f629c65b3664b82ed6ba3c630ddc0abacf51865
karimhaddad <karim.haddad@ircam.fr>
2022-03-14 12:10:04 +0100

    update release.bat (win) for LW8

--- OPENMUSIC/build/win/release.bat
+++ OPENMUSIC/build/win/release.bat
@@ -1,9 +1,9 @@
 
 cd ..
 
-"C:\\Program Files (x86)\\LispWorks\\lispworks-7-1-0-x86-win32.exe" -init deliver.lisp
+"C:\\Program Files (x86)\\LispWorks\\lispworks-8-0-0-x86-win32.exe" -init deliver.lisp
 
-"C:\\Program Files (x86)\\LispWorks\\lispworks-7-1-0-x86-win32.exe" -init pack-om.lisp
+"C:\\Program Files (x86)\\LispWorks\\lispworks-8-0-0-x86-win32.exe" -init pack-om.lisp
 
 cd win
 
