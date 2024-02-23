83e22b9df168f59b158920bd395202ce7b35cc9c
Anders Vinjar <anders@avinjar.no>
2024-02-23 19:58:12 +0100

    typo

--- OPENMUSIC/code/projects/musicproject/classes/chordseq.lisp
+++ OPENMUSIC/code/projects/musicproject/classes/chordseq.lisp
@@ -44,7 +44,7 @@ CHORD-SEQ is defined with:
 - <lmidic>: a list of list of pitches (midicents: 100 = 1 half-tone - 6000 = C3); e.g. '((6000 6500) (6100 6700 7100)). Each pitch list is considered as a chord in teh sequence. Single-note sequences can be specified with simple list of pitches, e.g. (6000 61000 6800) ; in this case each pitch will be considered as a single-note chord.
 - <lonsets>: a list of onsets in milliseconds (one for each chord). If the list is shorter than the list of pitch, the last interval is repeated.
 - <ldur>: a list or list of lists values (in milliseconds) expressing chords durations or note durations inside each chord.
-- <lvel>: a list or list of lists of values (MIDI velocity from 0 to 127) expressing chords velocities or note durations inside each chord.
+- <lvel>: a list or list of lists of values (MIDI velocity from 0 to 127) expressing chords velocities.
 - <loffset>: a list or list of lists of values (milliseconds) expressing note offsets for the notes inside each chord.
 - <lchan>: a list or list of lists of values (1-16) expressing MIDI channels for each chord or notes inside each chord.
 - <legato>: a number between 0 and 100, indicating the duration of chords as a percentage of inter-onsets time intervals. If different from 0 (the default), the ldur and loffset inputs are ignored, and notes in the chords are formatted with regard to the legato value.
