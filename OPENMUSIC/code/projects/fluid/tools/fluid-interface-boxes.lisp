9fd2f95d3309ed18e941380ec9a3d7774f8aca21
karimhaddad <karim.haddad@ircam.fr>
2024-05-05 17:09:06 +0200

    list method for fl-pgm

--- OPENMUSIC/code/projects/fluid/tools/fluid-interface-boxes.lisp
+++ OPENMUSIC/code/projects/fluid/tools/fluid-interface-boxes.lisp
@@ -205,6 +205,9 @@
 (defmethod propagate-pgm-change ((self number) value) ;self -> port, value -> pgm number
     (fluid-pgm-change value  '(1 2 3 4 5 6 7 8 9 11 12 13 14 15 16) :port self))
 
+(defmethod propagate-pgm-change ((self list) value) ;self -> port, value -> pgm number
+  (loop for i in self
+   do (fluid-pgm-change value  '(1 2 3 4 5 6 7 8 9 11 12 13 14 15 16) :port i)))
 
 ;==================
 ;FLUID-GAIN
