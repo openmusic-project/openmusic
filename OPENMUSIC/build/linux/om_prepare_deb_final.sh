cd79155e400473fb0bfe9488f0d61e9607e3ede9
Karim Haddad <karim.haddad@ircam.fr>
2025-09-24 11:41:35 +0200

    beta3

--- OPENMUSIC/build/linux/om_prepare_deb_final.sh
+++ OPENMUSIC/build/linux/om_prepare_deb_final.sh
@@ -5,9 +5,9 @@
 # @author   Karim Haddad
 #==============================================================================
 
-OM_VERSION="7.7-b.2"
+OM_VERSION="7.7-b.3"
 
-VERSION="7.7-b.2"
+VERSION="7.7-b.3"
 REVISION="1"
 ARCH="amd64"
 DEB_VERSION=${VERSION}-${REVISION}_${ARCH}
