eddbd0779a5a7f2eb8a713a4c31afb7af5259659
Karim Haddad <karim.haddad@ircam.fr>
2026-03-30 14:15:52 +0200

    updated build win64 bat file

--- OPENMUSIC/build/win64/release.bat
+++ OPENMUSIC/build/win64/release.bat
@@ -10,9 +10,9 @@ DEL libsdif.so
 
 CHDIR ..\..\..\build
 
-"C:\\Program Files\\LispWorks\\lispworks-8-0-0-x64-windows.exe" -init deliver.lisp
+"C:\\Program Files\\LispWorks\\lispworks-8-1-0-x64-windows.exe" -init deliver.lisp
 
-"C:\\Program Files\\LispWorks\\lispworks-8-0-0-x64-windows.exe" -init pack-om.lisp
+"C:\\Program Files\\LispWorks\\lispworks-8-1-0-x64-windows.exe" -init pack-om.lisp
 
 cd win64
 
